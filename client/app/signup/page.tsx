3d5584dddfa4e15de37a5744165494d7a1cc715c
henritresor <shimwamanahenritresor@gmail.com>
2023-08-08 15:33:49 +0200

    created login and signup pages

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -3,26 +3,28 @@ import Link from 'next/link'
 
 type Props = {}
 
-function Login({}: Props) {
+function Signup({}: Props) {
   return (
       <div className='w-full min-h-screen flex items-center justify-center'>
           <div className='shadow-md p-2 border'>
-              <h1 className='text-[2rem] text-center'>Login</h1>
+              <h1 className='text-[2rem] text-center'>Signup</h1>
 
               <div className='input-container'>
                   <label htmlFor="email">Email address: </label>
                   <input type="email" name='email' placeholder='email' id='email' />
               </div>
               <div className='input-container'>
-                  <label htmlFor="password">Password: </label>
-                  <input type="password" name='password' placeholder='enter password' id='password' />
+                  <label htmlFor="username">Username: </label>
+                  <input type="text" name='username' placeholder='username' id='username' />
               </div>
-              <a><Link href={'forgot password'}>Forgot password?</Link></a>
+              <div className='input-container'>
+                  <label htmlFor="password">Create Password: </label>
+                  <input type="password" name='password' placeholder='password' id='password' />
+              </div>
               <button className='p-2 border bg-blue-400 rounded-sm text-white mt-5'>
                   Sign in
               </button>
-              <p>Don&apos;t have an account yet? <a><Link href={'/signup'}>Create One!</Link></a></p>
-
+              <p>Already have an account? <a><Link href={'/login'}>Login</Link></a></p>
           </div>
 
         
@@ -30,4 +32,4 @@ function Login({}: Props) {
   )
 }
 
-export default Login
+export default Signup
